0a2856e3c325207fefdbc9cbf7aef3d4a494c67c
alxjnr <alexrobertjoiner@gmail.com>
2023-02-20 11:27:50 +0000

    begin testing the endpoint GET /api/categories

---
+++ __tests__/app.test.js
@@ -0,0 +1,36 @@
+const { app } = require("../app.js");
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const connection = require("../db/connection");
+const data = require("../db/data/test-data/index");
+
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  connection.end();
+});
+
+describe("API Testing", () => {
+  describe("Categories", () => {
+    describe("Get Requests", () => {
+      test("GET /api/categories should return a status code of 200", () => {
+        return request(app).get("/api/categories").expect(200);
+      });
+      test("GET /api/categories should return an object containing an array of category objects", () => {
+        return request(app)
+          .get("/api/categories")
+          .expect(200)
+          .then((res) => {
+            res.body.categories.forEach((obj) => {
+              expect(obj).toMatchObject({
+                description: expect.any(String),
+                slug: expect.any(String),
+              });
+            });
+          });
+      });
+    });
+  });
+});
