51bd312c8f0291cc4262ce9c6f8b668dc47e1c3e
alxjnr <alexrobertjoiner@gmail.com>
2023-02-20 15:37:09 +0000

    complete GET requests for review_id parametric endpoints

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const app = express();
 const { getCategories } = require("./controllers/controllers.categories");
+const { getReviewById } = require("./controllers/controllers.reviews");
 
 app.get("/api/categories", getCategories);
+
+app.get("/api/reviews/:review_id", getReviewById);
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send("Invalid type for request");
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send(err.msg);
+  } else {
+    next(err);
+  }
+});
 
 module.exports = { app };
